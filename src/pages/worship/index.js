d6c5d6c6331e19916f1c5d578e78f5d3df5c460e
Scott Blumenthal <scott.blumenthal@nytimes.com>
2020-09-12 12:41:06 -0400

    edits to worship page

--- src/pages/worship/index.js
+++ src/pages/worship/index.js
@@ -1,124 +1,63 @@
 import React from "react"
 import CoreLayout from "../../components/coreLayout"
 
-const AboutPage = () => (
+const WorshipPage = () => (
   <CoreLayout withSubtitle={false}>
     <section>
-      <h2>A Brief History of Morningside Monthly Meeting</h2>
+      <h2>Weekly Meeting</h2>
       <p>
-        From our origins as an informal worship group meeting on the campus of
-        Columbia University in the winter of 1957-58, Morningside grew into a
-        preparatory meeting under the care of New York Quarterly Meeting (1960),
-        then blossomed into a monthly meeting in 1973, and finally became fully
-        self-supporting in 1990. In July of 2002, we moved from the Columbia
-        campus to the tower of Riverside Church, and have been “permanently” at
-        home on the twelfth floor since 2007. For most of our history, we have
-        maintained a fairly constant number of members (55-60).
+        We gather for worship each First Day (Sunday) at 11:00am on the 12th
+        floor of the tower in Riverside Church in New York City.
+      </p>
+      <p>Our weekly worship adheres to the following pattern:</p>
+      <h3>Worship</h3>
+      <p>
+        Unprogrammed Quaker worship follows from our belief that each of us is
+        capable of a direct, unmediated experience of the Eternal Presence. It
+        is characterized by a deep and expectant silence, during which each
+        worshipper aims to free her/himself from the distractions of daily life.
+        This “centering down” allows us to be more fully alive to the Spirit.
+        Occasionally, out of this silence, one or more individuals may be led to
+        rise and share a message discerned to be intended for all those
+        gathered. Messages that do arise are not intended to provoke discussion,
+        but further inward reflection. Hence, it is our custom to follow each
+        message with a period of silence.
       </p>
       <p>
-        Unencumbered by the need to attend to the upkeep of a physical home, we
-        are free to devote our resources to outreach, and to concerns for peace
-        and social justice. A significant portion of our annual budget is
-        distributed, via our Peace and Social Concerns Committee, to a variety
-        of organizations who work to improve the quality of life of those whom
-        they serve. We tend to focus on small organizations, for whom our modest
-        contributions will make a noticeable difference, and on organizations
-        that serve the local community, regardless of their religious
-        affiliation.
+        Meeting for worship begins when the first worshipper enters the room and
+        begins to settle into the Silence. Its conclusion is signaled by a
+        handshake initiated by the meeting coordinator.
+      </p>
+      <h3>Afterwords</h3>
+      <p>
+        Worship continues with a period called Afterwords, which provides an
+        opportunity for us to build community by sharing messages of a more
+        personal nature: joys, sorrows, and brief reflections that did not rise
+        to the level of being shared during the main portion of worship. We ask
+        that messages be kept brief, and that a worshipful atmosphere be
+        maintained by allowing silence between messages, rather than lapsing
+        into conversation.
+      </p>
+      <h3>Introductions</h3>
+      <p>
+        Following Afterwords, the meeting coordinator will invite members of the
+        Morningside Ministry and Counsel Committee who are present to introduce
+        themselves. They welcome your questions about Quakerism in general and
+        Morningside in particular.
       </p>
       <p>
-        Our lack of a Meetinghouse has presented particular challenges as we
-        have labored together to create a loving community. We have met those
-        challenges by establishing spiritual nurture groups, conducting mid-week
-        meetings in each others’ homes, holding periodic retreats, and forming
-        discussion groups on a wide variety of topics.
+        At this point, visitors, newcomers, and those who are returning after a
+        prolonged absence will be invited to rise and introduce themselves, in
+        order that they may be greeted more fully during our Fellowship Hour.
       </p>
+      <h3>Announcements</h3>
       <p>
-        Our lack of a Meetinghouse has presented particular challenges as we
-        have labored together to create a loving community. We have met those
-        challenges by establishing spiritual nurture groups, conducting mid-week
-        meetings in each others’ homes, holding periodic retreats, and forming
-        discussion groups on a wide variety of topics.
+        Friends are invited to make announcements about events of interest to
+        the community.
       </p>
-      <p>
-        It is believed that Morningside was the first Meeting anywhere to take a
-        same-sex marriage under its care with the marriage of John Bohne and
-        William McCann on May 30, 1987. Other meetings had held “Ceremonies of
-        Commitment” for same-sex couples, but Morningside was the first to
-        affirm the right of all couples to name their relationships as they
-        choose.
-      </p>
-      <p>
-        Support committees for those among us who are facing personal
-        difficulties – or are embarking on new personal, professional, or
-        spiritual journeys – are regularly established by our Committee on
-        Ministry and Counsel.
-      </p>
-      <p>
-        While attendance at our First Day School has fluctuated over the years,
-        we are always prepared to welcome children, and work together to find
-        ways to introduce them to a life lived in the Light, in accordance with
-        the Quaker testimonies of equality, simplicity, integrity, and peace.
-      </p>
-    </section>
-    <section>
-      <h2>A Brief History of the Quakers</h2>
-      <p>
-        The term “quakerism” is believed to be a term of derision given to
-        17th-century Friends because they “quaked” before the Spirit. Like many
-        religious groups, they were persecuted because they rejected the
-        standard structures of the established church and forms of worship.
-      </p>
-      <p>
-        George Fox and other important early Friends, including Margaret Fell
-        (who would marry Fox) and James Nayler, believed that a priestly class,
-        vestments and religious symbols stood in the way of true worship.
-        Referring to John 15:15, early Quakers saw themselves as “friends” to
-        that of God in all.{" "}
-      </p>
-      <p>
-        The seminal Quaker writing from this period is the Journal or
-        Autobiography of George Fox, which describes the spiritual journey of a
-        young man who, failing to discover answers in the existing (state)
-        religion, found solace in silent worship, replacing theological
-        questions with personal experience and continuing revelation.{" "}
-      </p>
-      <p>
-        18th century Friends were led to see the inherent contradiction between
-        the freedom of the Spirit and the ownership of human beings, and to
-        begin questioning all moral argumens for slavery. Leaders like John
-        Woolman, who, in The Journal of John Woolman (1774), recounted his
-        40-year campaign to persuade slave-owning Friends to free their slaves,
-        emphasized that the fact that God was within us all was inconsistent
-        with the ownership of slaves. Woolman also saw dress as a socioeconomic
-        statement. He refused to wear cotton and eliminated collars and dyed
-        fabric from his wardrobe, not in order to create a Quaker aesthetic, but
-        because cotton and buttons and dyes were produced with slave labor and
-        wearing those products validated the system of slavery.
-      </p>
-      <p>
-        In the 19th century Friends experienced a period of Quietism in which
-        they ceased any activity in the political arena and struggled to
-        establish a shared identity. In the end Friends split into two sects
-        largely along economic and social lines. That schism between evangelical
-        and non-evangelical Friends continues to this day.
-      </p>
-      <p>
-        In the 20th Century writers such as Rufus Jones reinvigorated Quakerism
-        by framing it as simultaneously mystical and practical. Jones was one of
-        the founders, in 1917 of the American Friends Service Committee (AFSC),
-        to provide alternative service programs for conscientious objectors in
-        World War I. His combination of mysticism and service has come to define
-        Quakerism today. An excellent collection of digitized works of Jones and
-        other Quakers is available at HathiTrush.org. In 1943, a group of
-        Friends founded the Friends Committee on National Legislation (FCNL) in
-        order to lobby for changes in national legislation to make the nation's
-        laws more consistent with Quaker testimonies and values, especially the
-        peace testimony. FCNL is a “nonpartisan, multi-issue advocacy that
-        connects historic Quaker testimonies on peace, equality, simplicity, and
-        truth with peace and social justice issues.”
-      </p>
+      <h3>Fellowship</h3>
+      <p>All are welcome to share refreshments and conversation.</p>
     </section>
   </CoreLayout>
 )
-export default AboutPage
+export default WorshipPage
