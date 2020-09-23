2d36b6738964b92e3e19a4c65742dea58878ab1b
Scott Blumenthal <scott.blumenthal@nytimes.com>
2020-09-23 18:37:38 -0400

    I rendered an eventgit add .

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -29,48 +29,81 @@ function getGapi() {
   })
 }
 
-async function loadCalendar() {
-  try {
-    const gapi = await getGapi()
-    gapi.load("client:auth2", () => {
-      gapi.client
-        .init({
-          apiKey: API_KEY,
-          clientId: CLIENT_ID,
-          discoveryDocs: DISCOVERY_DOCS,
-          scope: SCOPES,
-        })
-        .then(
-          function () {
-            gapi.client.calendar.events
-              .list({
-                calendarId:
-                  "d1kb6t7loimf7b9ib89to5d2hs@group.calendar.google.com",
-                timeMin: new Date().toISOString(),
-                showDeleted: false,
-                singleEvents: true,
-                maxResults: 10,
-                orderBy: "startTime",
-              })
-              .then(function (response) {
-                console.log("foooo", response.result.items)
-              })
-          },
-          function (error) {
-            console.log("had a problem", error)
-          }
-        )
-    })
-  } catch (e) {
-    console.log(e)
-  }
+async function fetchCalendar() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const gapi = await getGapi()
+      gapi.load("client:auth2", () => {
+        gapi.client
+          .init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES,
+          })
+          .then(
+            function () {
+              gapi.client.calendar.events
+                .list({
+                  calendarId:
+                    "d1kb6t7loimf7b9ib89to5d2hs@group.calendar.google.com",
+                  timeMin: new Date().toISOString(),
+                  showDeleted: false,
+                  singleEvents: true,
+                  maxResults: 10,
+                  orderBy: "startTime",
+                })
+                .then(function (response) {
+                  console.log("got some events", response.result.items)
+                  resolve(response.result.items)
+                })
+            },
+            function (error) {
+              console.log("had a problem", error)
+              reject(error)
+            }
+          )
+      })
+    } catch (error) {
+      console.log(error)
+      reject(error)
+    }
+  })
 }
 
 const CalendarPage = () => {
+  const [events, setEvents] = useState([])
+  const [hasFetchedEvents, setHasFetchedEvents] = useState(false)
+
+  async function fetchCalendarEvents() {
+    if (hasFetchedEvents) {
+      return
+    }
+    const loadedEvents = await fetchCalendar()
+    setHasFetchedEvents(true)
+    setEvents(loadedEvents)
+  }
+
+  function renderEvents() {
+    if (hasFetchedEvents) {
+      return !events.length ? (
+        <p>There are no upcoming events on the calendar.</p>
+      ) : (
+        <ul>
+          {events.map(({ summary, id, start, end }) => {
+            return <li key={id}>{summary}</li>
+          })}
+        </ul>
+      )
+    } else {
+      return <p>Loading events from calendar...</p>
+    }
+  }
+
   useEffect(() => {
-    console.log("Here I am, Fooboy.")
-    loadCalendar()
+    fetchCalendarEvents()
   })
+
   return (
     <React.Fragment>
       <Helmet>
@@ -79,6 +112,7 @@ const CalendarPage = () => {
       <CoreLayout withSubtitle={false}>
         <section>
           <h2>Calendar</h2>
+          {renderEvents()}
         </section>
       </CoreLayout>
     </React.Fragment>
