a02e279f3fe0da7297aed9e01f5f358c7d54e222
Scott Blumenthal <scott.blumenthal@nytimes.com>
2020-12-10 21:15:23 -0500

    this should work

--- src/components/files/index.js
+++ src/components/files/index.js
@@ -7,16 +7,14 @@ import Styles from "./files.module.scss";
 const API_KEY = "AIzaSyCCOtjPgXJ5tIqEILv9gm5pCpOAbyV_3aY";
 const CLIENT_ID =
   "1026426394881-dbessqt7532lnu3j8evh83qicmg6uhak.apps.googleusercontent.com";
-const DISCOVERY_DOCS = [
-  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
-];
-const SCOPES = "GET https://www.googleapis.com/drive/v3/files";
+const DISCOVERY_DOCS =
+  "https://content.googleapis.com/discovery/v1/apis/drive/v3/rest";
+const DEFAULT_FIELDS = [];
 
 // These can be imported by pages using the calendar component, so they can
 // refer by key to the one they'd like to include
-export const CALENDAR_IDS = {
-  main: "d1kb6t7loimf7b9ib89to5d2hs@group.calendar.google.com",
-  psc: "dr4fmp2quj73h6i65ul5v53bco@group.calendar.google.com",
+export const FOLDER_IDS = {
+  business_minutes: "1z84XpKreg6-MZlShXk8_svqQQYX4R24z",
 };
 
 function getGapi() {
@@ -39,60 +37,33 @@ function getGapi() {
   });
 }
 
-async function fetchDrive(maxResults = 10) {
+async function fetchDrive(folderId, pageSize = 100) {
   return new Promise(async (resolve, reject) => {
     try {
       const gapi = await getGapi();
       gapi.load("client:auth2", () => {
         gapi.auth2.init({ client_id: CLIENT_ID }).then(function () {
           gapi.client.setApiKey(API_KEY);
-          gapi.client
-            .load(
-              "https://content.googleapis.com/discovery/v1/apis/drive/v3/rest"
-            )
-            .then(function () {
-              gapi.client.drive.files
-                .list({
-                  q: "'1z84XpKreg6-MZlShXk8_svqQQYX4R24z' in parents",
-                })
-                .then(
-                  function (response) {
-                    // Handle the results here (response.result has the parsed body).
-                    console.log("Response", response);
-                    resolve(response);
-                  },
-                  function (err) {
-                    console.error("Execute error", err);
-                    reject(err);
-                  }
-                );
-            });
+          gapi.client.load(DISCOVERY_DOCS).then(function () {
+            gapi.client.drive.files
+              .list({
+                q: `'${folderId}' in parents`,
+                pageSize,
+                fields:
+                  "files(id, name, description, starred, mimeType, webViewLink, createdTime, modifiedTime)",
+              })
+              .then(
+                function (response) {
+                  // Handle the results here (response.result has the parsed body).
+                  resolve(response.result.files);
+                },
+                function (err) {
+                  console.error("Execute error", err);
+                  reject(err);
+                }
+              );
+          });
         });
-        // gapi.client
-        //   .init({
-        //     apiKey: API_KEY,
-        //     clientId: CLIENT_ID,
-        //     discoveryDocs: DISCOVERY_DOCS,
-        //     scope: SCOPES,
-        //   })
-        //   .then(
-        //     function () {
-        //       gapi.client.drive.files
-        //         .list({
-        //           corpora: "drive",
-        //           pageSize: 10,
-        //           fields: "nextPageToken, files(id, name)",
-        //         })
-        //         .then(function (response) {
-        //           // console.log("got some events", response.result.items);
-        //           resolve(response.result.files);
-        //         });
-        //     },
-        //     function (error) {
-        //       console.log("had a problem", error);
-        //       reject(error);
-        //     }
-        //   );
       });
     } catch (error) {
       console.log(error);
@@ -101,7 +72,7 @@ async function fetchDrive(maxResults = 10) {
   });
 }
 
-const Files = () => {
+const Files = ({ folderIds = [FOLDER_IDS.business_minutes] }) => {
   const [files, setFiles] = useState([]);
   const [hasFetchedFiles, setHasFetchedFiles] = useState(false);
 
@@ -110,15 +81,17 @@ const Files = () => {
       return;
     }
 
-    const driveFiles = await fetchDrive();
-    // await Promise.all(
-    //   calendarIds.map(async (calendarId) => {
-    //     const calendarEvents = await fetchDrive(calendarId);
-    //     loadedEvents = loadedEvents.concat(...calendarEvents);
-    //   })
-    // );
+    let loadedFiles = [];
+    await Promise.all(
+      folderIds.map(async (folderId) => {
+        const folderFiles = await fetchDrive(folderId);
+        console.log(folderFiles);
+        loadedFiles = loadedFiles.concat(...folderFiles);
+      })
+    );
 
     setHasFetchedFiles(true);
+    setFiles(loadedFiles);
     // const groupedEvents = groupEvents(loadedEvents);
     // setEvents(groupedEvents);
   }
@@ -166,63 +139,60 @@ const Files = () => {
     return format(date, "h:mm aaaa");
   }
 
-  function renderEvent({
-    summary,
-    id,
-    location,
-    description,
-    start,
-    end,
-    organizer,
-  }) {
-    const { email: calendarId } = organizer;
-    const startDate = parseEventISODate(start);
-    const endDate = parseEventISODate(end);
-    const startDayStr = format(startDate, "yyyy-MM-dd");
-    const isSameDay = startDayStr === format(endDate, "yyyy-MM-dd");
-    const displaySpan = isSameDay
-      ? `${displayDay(startDate)}, ${displayTime(startDate)} to ${displayTime(
-          endDate
-        )}`
-      : `${displayDay(startDate)}, ${displayTime(startDate)} to ${displayDay(
-          endDate
-        )}, ${displayTime(endDate)}`;
+  function renderFile({ id, name, webViewLink, description }) {
     return (
-      <li key={id}>
-        <header>{summary}</header>
-        <time dateTime={startDayStr}>{displaySpan}</time>
-        {location ? <span>{location}</span> : null}
-        {description ? (
-          <p dangerouslySetInnerHTML={{ __html: description }}></p>
-        ) : null}
-        {calendarId != CALENDAR_IDS.main ? (
-          <footer>{organizer.displayName}</footer>
-        ) : null}
-      </li>
+      <p key={id}>
+        <a href={webViewLink}>{name}</a>: {description}
+      </p>
     );
   }
 
-  function renderEvents() {
-    // if (hasFetchedFiles) {
-    //   if (!events.size) {
-    //     return <p>There are no upcoming events on the calendar.</p>;
-    //   } else {
-    //     const months = [];
-    //     const eventsByMonth = events.entries();
-    //     for (let i = 0; i < events.size; i++) {
-    //       const [month, evts] = eventsByMonth.next().value;
-    //       months.push(
-    //         <Fragment key={`month-${month}`}>
-    //           <h3>{month}</h3>
-    //           <ul className={Styles.calendarEvents}>{evts.map(renderEvent)}</ul>
-    //         </Fragment>
-    //       );
-    //     }
-    //     return months;
-    //   }
-    // } else {
-    //   return <p>Loading events from calendar...</p>;
-    // }
+  // function renderEvent({
+  //   summary,
+  //   id,
+  //   location,
+  //   description,
+  //   start,
+  //   end,
+  //   organizer,
+  // }) {
+  //   const { email: calendarId } = organizer;
+  //   const startDate = parseEventISODate(start);
+  //   const endDate = parseEventISODate(end);
+  //   const startDayStr = format(startDate, "yyyy-MM-dd");
+  //   const isSameDay = startDayStr === format(endDate, "yyyy-MM-dd");
+  //   const displaySpan = isSameDay
+  //     ? `${displayDay(startDate)}, ${displayTime(startDate)} to ${displayTime(
+  //         endDate
+  //       )}`
+  //     : `${displayDay(startDate)}, ${displayTime(startDate)} to ${displayDay(
+  //         endDate
+  //       )}, ${displayTime(endDate)}`;
+  //   return (
+  //     <li key={id}>
+  //       <header>{summary}</header>
+  //       <time dateTime={startDayStr}>{displaySpan}</time>
+  //       {location ? <span>{location}</span> : null}
+  //       {description ? (
+  //         <p dangerouslySetInnerHTML={{ __html: description }}></p>
+  //       ) : null}
+  //       {calendarId != CALENDAR_IDS.main ? (
+  //         <footer>{organizer.displayName}</footer>
+  //       ) : null}
+  //     </li>
+  //   );
+  // }
+
+  function renderFiles() {
+    if (hasFetchedFiles) {
+      if (!files.length) {
+        return <p>No files found.</p>;
+      } else {
+        return files.map(renderFile);
+      }
+    } else {
+      return <p>Loading files...</p>;
+    }
   }
 
   useEffect(() => {
@@ -234,7 +204,7 @@ const Files = () => {
       <Helmet>
         <script async defer src="https://apis.google.com/js/api.js"></script>
       </Helmet>
-      <section>HI</section>
+      <section>{renderFiles()}</section>
     </React.Fragment>
   );
 };
