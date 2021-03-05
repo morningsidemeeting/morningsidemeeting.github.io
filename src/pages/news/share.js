374bb51b033c58fbd7c0f9aa66a8a158e7c228d4
Scott Blumenthal <scott.blumenthal@nytimes.com>
2021-03-05 06:48:36 -0500

    style cleanup.  links back to committees page for default committee section index text.

--- src/pages/news/share.js
+++ src/pages/news/share.js
@@ -9,9 +9,9 @@ export default function SharePage() {
           src="https://docs.google.com/forms/d/e/1FAIpQLSduWq8tRwHnHzr4_BFJ4v89ZYL6okhw31pAqawNUSDHpGsvGw/viewform?embedded=true"
           width="640"
           height="1700"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
           title="Share News Form"
           style={{ margin: "0 auto", width: "640px", display: "block" }}
         >
