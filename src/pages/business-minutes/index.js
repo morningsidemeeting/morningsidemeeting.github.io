270948536997da8f39dbdf48a064f877b07bd123
Scott Blumenthal <scott.blumenthal@nytimes.com>
2021-03-13 15:57:14 -0500

    format file names for business meetings

--- src/pages/business-minutes/index.js
+++ src/pages/business-minutes/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import FilesPage from "../../components/files/filesPage";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function formatDate(match, p1, p2) {
+  return `${months[parseInt(p2) - 1]}, 20${p1}`;
+}
+
+function formatFileName(name) {
+  return name.replace(/(\d{2})-(\d{2}).pdf$/, formatDate);
+}
+
 export default function BusinessMinutesPage() {
-  return <FilesPage folderSlug="business_minutes" />;
+  return (
+    <FilesPage folderSlug="business_minutes" formatFileName={formatFileName} />
+  );
 }
