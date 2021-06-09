a6bfb22899747d8e85b6662866a48973f5810f28
Scott Blumenthal <scott.blumenthal@nytimes.com>
2021-06-08 20:26:36 -0400

    adding an ARCH  post and committee description on Charlene's behalf.

--- src/pages/arch/index.js
+++ src/pages/arch/index.js
@@ -7,8 +7,14 @@ export default function CommitteePage() {
   return (
     <Committee title={title} basePath={basePath}>
       <p>
-        Committee description coming soon. Please see the main{" "}
-        <Link to="/committees">Committees page</Link> for more details.
+        <a href="https://www.nyym.org/content/arch">ARCH</a> (Aging Resources
+        Consultation and Help) is a program of the New York Yearly Meeting. ARCH
+        visitors are trained to help Friends deal with aging or
+        disability-related concerns.
+      </p>
+      <p>
+        At Morningside Meeting, ARCH is a committee of the whole. Charlene Ray
+        is the convenor.
       </p>
     </Committee>
   );
