f0c1f2744b6f1fe3c6cae3d00f450f1204c990c9
Scott Blumenthal <scott.blumenthal@gmail.com>
2021-09-08 08:52:58 -0400

    adding nomcom description and link to the current position grid

--- src/pages/nominating/index.js
+++ src/pages/nominating/index.js
@@ -1,14 +1,44 @@
 import React from "react";
 import Committee from "../../components/committee";
 import { basePath, title } from "./config.json";
-import { Link } from "gatsby";
 
 export default function CommitteePage() {
   return (
     <Committee title={title} basePath={basePath}>
+      <p>The Morningside Nominating Committee:</p>
+      <ul>
+        <li>
+          Fills positions and committees in Monthly Meeting by asking people
+          what they would like to do, getting suggestions, and searching for
+          (possibly hidden) talents.
+        </li>
+
+        <li>
+          In December, presents nominations for positions and committees to
+          Business Meeting, for final approval in January.
+        </li>
+
+        <li>Meets as needed, but more frequently in the fall.</li>
+
+        <li>
+          Maintains a list of Morningside's committees and committee members.{" "}
+          <a href="https://docs.google.com/document/d/1JfwTA2Bjl9yX_4i98LLDJwOeWmneI9MU/edit?usp=drivesdk&ouid=0&rtpof=true&sd=true">
+            The current list can be found here.
+          </a>
+        </li>
+
+        <li>
+          Members of Nominating Committee who are members of the Society of
+          Friends also serve on Quarterly Meeting Nominating Committee, which
+          meets about four times per year.
+        </li>
+      </ul>
       <p>
-        Committee description coming soon. Please see the main{" "}
-        <Link to="/committees">Committees page</Link> for more details.
+        Members of the Nominating Committee are themselves nominated by a Naming
+        Committee (which is established/confirmed by Ministry and Counsel). The
+        naming committee brings names to the business meeting. In January 2021,
+        Charlene Ray and Jason McGill were confirmed by Ministry and Counsel as
+        a Naming Committee, to be brought to meeting for business for approval.
       </p>
     </Committee>
   );
