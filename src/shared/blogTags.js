b5e58ae9ae94b9a441fd4f95d8061642f9d9e0bc
Scott Blumenthal <scott.blumenthal@nytimes.com>
2022-06-24 16:43:13 -0400

    Annual appeal letter

--- src/shared/blogTags.js
+++ src/shared/blogTags.js
@@ -1,13 +1,14 @@
 export const BLOG_TAGS = {
-  psc: "Peace & Social Concerns Committee",
   announcements: "Announcements",
+  arch: "ARCH",
   blog: "Blog Posts",
-  prisons: "Prisons Committee",
-  "ministry-and-counsel": "Ministry and Counsel",
-  communications: "Communications and Website Commitee",
   board: "Bulletin Board",
+  communications: "Communications and Website Commitee",
+  finance: "Finance Committee",
+  "ministry-and-counsel": "Ministry and Counsel",
+  prisons: "Prisons Committee",
+  psc: "Peace & Social Concerns Committee",
   rjsg: "Racial Justice Study Group",
-  arch: "ARCH"
 };
 
 export default BLOG_TAGS;
