a993cf5c0289d645b0bb56cf9894b8b77b66f313
Scott Blumenthal <scott.blumenthal@gmail.com>
2022-09-07 08:01:25 -0400

    urgent call post and HTML version

---
+++ content/posts/board-urgent-call.md
@@ -0,0 +1,11 @@
+---
+title: An Urgent Call to the Religious Society of Friends, May 2022
+date: 2022-09-07
+tags: [board, communications]
+---
+
+Art Moritz shares:
+
+Some members and attenders of Morningside Monthly Meeting have been informally discussing the subject "Urgent Call". Its text is available from the sponsors only as a [downloadable PDF file](https://img1.wsimg.com/blobby/go/53cdbcd4-0cda-4cfe-b4c7-3a9a2892e5b9/UrgentCall_toRSoF-FINAL.pdf). As such, it is not smartphone friendly.
+
+Our Communications Committee has made an [HTML version](/communications/urgentcall) available on the website so that our members and attenders can more easily view the "Urgent Call" on their phones.
---
+++ src/pages/communications/urgentcall.js
@@ -0,0 +1,353 @@
+import React from "react";
+import CoreLayout from "../../components/coreLayout";
+import SEO from "../../components/seo";
+import Styles from "./urgentcall.module.scss";
+
+const UrgentCallPage = () => (
+  <CoreLayout withSubtitle={false}>
+    <SEO title="An Urgent Call to the Religious Society of Friends" />
+    <section>
+      <h2>An Urgent Call to the Religious Society of Friends</h2>
+      <p>May 2022</p>
+      <p>
+        We Quakers, members of the Religious Society of Friends, hold strongly
+        to the principle of speaking truth with integrity. We ground our
+        speaking truth in our worship and our searching of ourselves, as we
+        listen deeply and honestly within and across all differences. Our
+        testimony to the world includes standing up for spiritually discerned
+        Truth, the equality of all persons, the peaceful resolution of
+        conflicts, and respect and care for our neighbors and the earth. Right
+        now, we see many forces at work in our country and the world that are
+        undermining these basic values. Accordingly, we call on Friends
+        everywhere to act.
+      </p>
+      <p>
+        In this country, in 2020, we witnessed an attempted coup. The January 6
+        assault on the Capitol was a deliberate, violent attempt to prevent Joe
+        Biden — the clear winner of the 2020 election — from taking office and
+        keeping then-president Donald Trump in power. To maintain that the last
+        election was riddled with fraud is demonstrably false. This politically
+        motivated movement has now embarked on an orchestrated campaign to
+        suppress voting by black, brown and young people and to pass new state
+        election laws giving the power to throw out huge numbers of valid
+        ballots to highly partisan legislatures, governors, and state attorneys
+        general. Thus, this lie is being used to deprive people of their
+        constitutional rights and constitutes a major injustice.
+      </p>
+      <p>
+        As Friends, we feel called to speak out against these falsehoods and
+        anti-democratic actions. We encourage Friends to seek spiritual guidance
+        on any actions that you and your meetings might take to witness against
+        this gross injustice. If this politically motivated movement succeeds in
+        its anti-democratic takeover attempt, any hope of making significant
+        progress on racial and economic justice, the equality of all people, the
+        climate crisis, gun violence, and other urgent issues of our time will
+        be gone.
+      </p>
+      <p>
+        We know that democracy in the United States has many flaws. During the
+        past 100 years, our local, state and federal governments have acted to
+        prevent formerly enslaved people and other communities of color from
+        voting. It was not until 1920 that women finally won the right to vote,
+        and their equality is still being challenged. We call on Friends to act
+        — first, to resist all efforts to undermine our current democratic
+        processes, and second, to support true democratic reforms to our still
+        flawed election system. We cannot allow a government that is built on
+        lies and injustice.
+      </p>
+      <p>
+        We the undersigned are encouraging Quakers throughout the country to
+        work in their local communities, states, and the national arena to take
+        critical actions to prevent an authoritarian takeover of our democracy.
+        We urge Friends to discern the deep truths that provide a foundation for
+        active love, wisdom, compassion, and a truly democratic electoral
+        system—and then to actively support such changes. We know that love
+        conquers fear. Let us lean into Divine Love and find ways forward in
+        Truth.
+      </p>
+      <p>
+        On the reverse are two queries for you and your meeting to consider in
+        response to this call. We’ve also included a list of specific actions
+        that Friends could begin taking now. Some actions may become necessary
+        later if there is another attempted coup during the next presidential
+        election cycle.
+      </p>
+    </section>
+    <section className={Styles.queries}>
+      <h2>Queries</h2>
+      <ol>
+        <li>
+          What, if anything, is the Light Within calling Friends to do in
+          response to the unprecedented rise of domestic extremism, white
+          supremacy, and authoritarianism that is threatening the destruction of
+          our democracy at home and abroad?
+        </li>
+        <li>
+          How can I/we respond with resolute love even while I/we may be
+          struggling with fear, anger, apathy, or hate?
+        </li>
+      </ol>
+    </section>
+    <section>
+      <h3>Defending Truth</h3>
+      <p>
+        We urge all Friends to discern the deep truths that provide a foundation
+        for active love, wisdom, compassion, and peace in the world — and then
+        to defend them. We ask:
+      </p>
+      <ul>
+        <li>
+          Individual Friends to search yourselves about the threats to our
+          country, and the world, and to witness publicly to the truths that you
+          discern are grounded in your experience of the Spirit.
+        </li>
+        <li>
+          Quaker meetings and organizations to issue public statements calling
+          out lies and the purveyors of lies, all in the context of our
+          understanding of love, equality, and justice for all. Working for True
+          Equality and Justice for All We urge Friends to act for equality and
+          equity within our diverse society:
+        </li>
+        <li>
+          Join or support organizations led by people of color which are working
+          for the empowerment of black, brown, indigenous and Asian people and
+          communities.
+        </li>
+        <li>
+          Speak, write, and protest in support for full equality of all people
+          in American society, whatever their racial or sexual identity, gender,
+          or class.
+        </li>
+        <li>
+          Support legislative and economic changes that would lead to actual
+          equity among all peoples.
+        </li>
+      </ul>
+      <h3>Promoting Free and Fair Elections</h3>
+      <p>
+        Stemming from our testimonies of Integrity and Equality, we urge Friends
+        to:
+      </p>
+      <ul>
+        <li>
+          Support non-partisan voter-registration drives, particularly in
+          communities under attack from those seeking to prevent them from
+          voting.
+        </li>
+        <li>Publicly condemn partisan efforts to restrict access to voting.</li>
+        <li>
+          Confront partisan efforts to seize control of the election
+          administration apparatus.
+        </li>
+        <li>
+          Support conscientious, non-partisan election officials in our
+          communities.
+        </li>
+        <li>
+          Volunteer as poll workers, election monitors, and drivers to get
+          people to the polls.
+        </li>
+      </ul>
+      <h3>Preparing for Non-Violent Resistance</h3>
+      <p>
+        We believe that as a religious society grounded in nonviolence, we can:
+      </p>
+      <ul>
+        <li>
+          Initiate discussions on the impacts of voter suppression and new laws
+          enabling partisan actors to legally throw out ballots and change the
+          results of elections, then garnering support for nonviolent resistance
+          against all this, and forming community-based networks of support for
+          such actions.
+        </li>
+        <li>
+          Arrange for nonviolent direct-action training in our Quaker meetings
+          and organizations, as well as with others in our larger communities.
+        </li>
+        <li>
+          Form small “affinity groups” that can prepare to carry out direct
+          actions in support of free and fair elections and the state and local
+          officials who support them.
+        </li>
+        <li>
+          Support wider efforts to plan and organize nonviolent resistance on a
+          state and nationwide level, as needed.
+        </li>
+      </ul>
+    </section>
+
+    <section className={Styles.signers}>
+      <ul>
+        <li>
+          MARIAN BEANE
+          <br />
+          Charlotte Friends Meeting, Piedmont Friends - Yearly Meeting
+          <br />
+          and Fellowship
+        </li>
+
+        <li>
+          BRUCE BIRCHARD
+          <br />
+          Central Philadelphia Friends Meeting, Philadelphia Yearly
+          <br />
+          Meeting, Former General Secretary, Friends General Conference
+        </li>
+
+        <li>
+          LAURA BOYCE
+          <br />
+          Providence Friends Meeting, Philadelphia Yearly Meeting,
+          <br />
+          Ujima Friends Peace Center
+        </li>
+
+        <li>
+          MARTHA BRYANS
+          <br />
+          Downingtown Friends Meeting, Philadelphia Yearly Meeting
+        </li>
+
+        <li>
+          SAM CALDWELL
+          <br />
+          Providence Friends Meeting, Philadelphia Yearly Meeting,
+          <br />
+          Former General Secretary, Philadelphia Yearly Meeting
+        </li>
+
+        <li>
+          GRETCHEN CASTLE
+          <br />
+          Doylestown Friends Meeting, Philadelphia Yearly Meeting,
+          <br />
+          Dean, Earlham School of Religion, Former General Secretary,
+          <br />
+          Friends World Committee for Consultation World Office
+        </li>
+
+        <li>
+          ROBERT DOCKHORN
+          <br />
+          Green Street Friends Meeting, Philadelphia Yearly Meeting
+        </li>
+
+        <li>
+          TOM EWELL
+          <br />
+          Whidbey Island Friends Meeting, North Pacific Yearly Meeting
+        </li>
+
+        <li>
+          EILEEN FLANAGAN
+          <br />
+          Chestnut Hill Friends Meeting, Philadelphia Yearly Meeting
+        </li>
+
+        <li>
+          JOHN HELDING
+          <br />
+          Lopez Island Friends Meeting, North Pacific Yearly Meeting
+        </li>
+
+        <li>
+          AYESHA IMANI
+          <br />
+          Ujima Friends Meeting, Philadelphia, Clerk, Ujima Friends
+          <br />
+          Peace Center, Former Clerk of the Fellowship of Friends
+          <br />
+          of African Descent
+        </li>
+
+        <li>
+          PHIL LORD
+          <br />
+          Ujima Friends Meeting, Chestnut Hill Friends Meeting,
+          <br />
+          Philadelphia Yearly Meeting
+        </li>
+
+        <li>
+          MARY ELLEN MCNISH
+          <br />
+          Byberry Friends Meeting, Philadelphia Yearly Meeting, Former
+          <br />
+          General Secretary, American Friends Service Committee
+        </li>
+
+        <li>
+          DIANE RANDALL
+          <br />
+          Hartford Friends Meeting, New England Yearly Meeting,
+          <br />
+          Sojourning Friends Meeting of Washington, Baltimore Yearly
+          <br />
+          Meeting, Former General Secretary, Friends Committee on
+          <br />
+          National Legislation
+        </li>
+
+        <li>
+          CHRISTINA REPOLEY
+          <br />
+          Atlanta Friends Meeting, Southern Appalachian Yearly Meeting
+          <br />
+          and Association, Founding Director of Quaker Voluntary Service
+        </li>
+
+        <li>
+          COLIN SAXTON
+          <br />
+          North Valley Friends Meeting, Newberg, Oregon
+        </li>
+
+        <li>
+          JOE VOLK
+          <br />
+          Executive Secretary Emeritus, Friends Committee on
+          <br />
+          National Legislation
+        </li>
+
+        <li>
+          MICHAEL WAJDA
+          <br />
+          Goshen Friends Meeting, Philadelphia Yearly Meeting,
+          <br />
+          Sojourning Bennington Friends Meeting, New England
+          <br />
+          Yearly Meeting, Former Associate Secretary, Friends
+          <br />
+          General Conference
+        </li>
+
+        <li>
+          PAM YALLER
+          <br />
+          Upper Dublin Friends Meeting, Philadelphia Yearly Meeting
+        </li>
+
+        <p>
+          <em>
+            Meeting and Organization Affiliations are listed for identification
+            purposes only
+          </em>
+        </p>
+      </ul>
+    </section>
+
+    <section>
+      <p>
+        You can endorse this Quaker message, find additional resources, register
+        for national conversations on these issues, report on specific actions
+        that you, your meeting, or your organization has undertaken, and sign up
+        for continued updates at{" "}
+        <a href="https://quakercall.net/">www.quakercall.net</a>. We also
+        welcome your questions, suggestions, and resources at
+        <a href="mailto:info@quakercall.net">info@quakercall.net</a>.
+      </p>
+    </section>
+  </CoreLayout>
+);
+export default UrgentCallPage;
